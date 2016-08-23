3d8246a715ed6bac3a2ecc7a540ef335e9a141eb
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-08-23 18:35:17 +0200

    Router on index

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ header("Content-Type: text/html");
 
 include __DIR__ . '/vendor/autoload.php';
 
+use WCal\Controller\TemplateManager;
+use WCal\Controller\DataWorker;
+
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
@@ -10,21 +13,45 @@ $whoops->register();
 $router = new AltoRouter();
 $router->setBasePath('');
 
-// Setup the URL routing. This is production ready.
-$router->map('GET','/', 'src/View/home.php', 'home');
-$router->map('GET','/insert/', __DIR__.'/src/View/insert.php', 'save');
-$router->map('POST','/write/', __DIR__.'/src/Model/write.php', 'write');
-$router->map('GET','/data/', 'about.php', 'about');
-$router->map('GET','/test/', __DIR__.'/src/Controller/template-manager.php', 'temp');
+// Setup the URL routing.
+// 1-http request, 2-matching url, 3-path to match, 4-name
+$router->map('GET','/', function(){
+    $tmanager = new TemplateManager();
+    $tmanager->home();
+}, 'home');
 
-/* Match the current request */
+$router->map('GET', '/form/', function(){
+    $tmanager = new TemplateManager();
+    $tmanager->form();
+}, 'form');
+
+$router->map('GET', '/data/', function(){
+    $tmanager = new TemplateManager();
+    $tmanager->data();
+}, 'about');
+
+$router->map('GET', '/about/', function(){
+    $tmanager = new TemplateManager();
+    $tmanager->about();
+});
+
+$router->map('POST','/write/', function(){
+    $worker = new DataWorker();
+    $worker->saveData();
+});
+
+$router->map('GET', '/delete/[i:id]/' , function($id){
+    $worker = new DataWorker();
+    $worker->deleteData($id);
+});
+
 $match = $router->match();
-if($match) {
-  require $match['target'];
+
+# If a matching route is found
+if( $match && is_callable( $match['target'] ) ) {
+        call_user_func_array( $match['target'], $match['params']);
+} else {
+// no route was matched show 404 page
+    header("HTTP/1.0 404 Not Found");
+    require 'static/404.html';
 }
-else {
-  header("HTTP/1.0 404 Not Found");
-  // show 404 page
-  require __DIR__ . '/src/View/404.php';
-}
-?>
